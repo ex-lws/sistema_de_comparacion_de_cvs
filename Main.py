79e8503726df0e2284d268e1f8c8ddc45d5121e3
ex-lws <theleroybottom@gmail.com>
2025-05-12 19:38:52 -0600

    Mi primer commit: Subida inicial del proyecto

---
+++ Main.py
@@ -0,0 +1,9 @@
+# En esta clase se harán las pruebas del sistema en su versión de consola. 
+
+# Clases importatdas
+from src.ConectarBD import conectarBD
+
+def main():
+    # Aquí se pueden realizar pruebas de las clases y funciones definidas en el módulo
+    print ('Iniciando el sistema de comparación de CVs')
+    conectarBD()
---
+++ src/ConectarBD.py
@@ -0,0 +1,26 @@
+# Mediante esta clase se conecta a la base de datos postgreSQL 16.9
+
+# Se importan las librerías necesarias
+import psycopg
+from psycopg import OperationalError
+
+def conectarBD():
+    try:
+        conn = psycopg.connect(
+            # Credenciales de la conexion para la base de datos.
+            # Modificar según sea necesario.
+            dbname="PerfilesDBv1.0",
+            user="postgres",
+            password="admin",
+            host="localhost"
+        )
+        print("¡Conexión exitosa!")
+        return conn
+    except OperationalError as e:
+        print(f"Error al conectar: {e}")
+        return None
+
+# Ejemplo de uso
+conexion = conectarBD()
+if conexion:
+    conexion.close()
